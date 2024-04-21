65d0c8aa9dd0956269be48d18347749b3ff965f7
KHimanikrishna <163715050+KHimanikrishna@users.noreply.github.com>
2024-04-21 22:36:59 +0530

    Create constructFromPrePost.py

---
+++ constructFromPrePost.py
@@ -0,0 +1,17 @@
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not postorder or not preorder:
+            return None
+        if len(postorder) ==1:
+            return TreeNode(postorder[0])
+        if len(preorder) ==1:
+            return TreeNode(preorder[0])
+        head = TreeNode(preorder[0])
+        mid = preorder.index(postorder[-2])
+        postorderLeft = postorder[0:mid-1]
+        postorderRight = postorder[mid-1:-1]
+        preorderLeft = preorder[1:mid]
+        preorderRight = preorder[mid:]
+        head.left = self.constructFromPrePost(preorderLeft, postorderLeft)
+        head.right = self.constructFromPrePost(preorderRight, postorderRight)
+        return head
